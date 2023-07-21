e56775f531065e5af91bc8fa4f696790ce96b64a
Alekseev Aleksandr <Alekseev.7aa@yandex.ru>
2023-07-21 11:01:05 +0300

    v0.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken')
 module.exports = (req, res, next) => {
   const { authorization } = req.headers
   if (!authorization) {
-    return res.status(413).send('please log in')
+    return res.status(401).send({message:'please log in'})
   }
   const token = authorization.replace('Bearer ', '')
 
@@ -13,9 +13,8 @@ module.exports = (req, res, next) => {
   }
   catch (err) {
     if (err.name === 'JsonWebTokenError') return res.status(400).send('invalid token')
-    return res.status(403).send('please log in')
+    return res.status(401).send({message:'please log in'})
   }
   req.user = payload
-  console.log("ZALOGINEN");
   next()
 }
